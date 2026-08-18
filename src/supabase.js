3cd5ee0f6298a9a4bbaa6e05ecbe99156ee4cd93
Talyah Moses <talyah.moses@entelect.co.za>
2026-08-18 20:22:50 +0200

    Initial commit - Option A setup

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://fetpncdjrmfknofvekqc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZldHBuY2Rqcm1ma25vZnZla3FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4MDE3NDEsImV4cCI6MjEwMjM3Nzc0MX0.oiKiVJ8u18g0G4XOmjcBaDGdezRwVuMqmVRI3e80V0E';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
